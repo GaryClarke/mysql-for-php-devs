54886a58a92a2f41d4fa30c7acf489ab151fe14d
Gary Clarke <garyclarke1974@gmail.com>
2024-10-14 17:04:58 +0100

    Correct query

--- sql/where-in.sql
+++ sql/where-in.sql
@@ -1,6 +1,6 @@
 -- Select all employees who are in either 'Sales' or 'IT' departments
-SELECT name, department FROM employees
-WHERE department_id IN (SELECT department_id FROM departments WHERE name IN ('Sales', 'IT'));
+SELECT name, department_id FROM employees
+WHERE department_id IN (SELECT id FROM department WHERE name IN ('Sales', 'IT'));
 
 -- List all employees (just name) from departments with more than three employees
 SELECT name
