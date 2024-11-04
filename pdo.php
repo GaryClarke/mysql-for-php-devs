58f0744e40c7cf1f7b2d575ede1d90d2c67e1848
Gary Clarke <garyclarke1974@gmail.com>
2024-11-04 08:19:54 +0000

    Create pdo.php

---
+++ pdo.php
@@ -0,0 +1,13 @@
+<?php
+
+// Connect to MySQL using PDO
+try {
+    $dsn = "mysql:host=mysql;dbname=course_demo";
+    $username = "user";
+    $password = "studentpassword";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully using pdo";
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
