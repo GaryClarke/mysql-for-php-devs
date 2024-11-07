b2d0aa37fcfcf4f613ccd5a213ccac91d2bf9a60
Gary Clarke <garyclarke1974@gmail.com>
2024-11-07 19:06:33 +0000

    Create pdo-creating.php

--- PDO_CRUD/pdo-creating.php
+++ PDO_CRUD/pdo-creating.php
@@ -2,15 +2,19 @@
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
-$db = Database::getInstance();
+$db = \App\DB\Database::getInstance();
 $pdo = $db->getPDO();
 
-$stmt = $pdo->prepare("SELECT id, name, department FROM employees");
+$stmt = $pdo->prepare("SELECT id, name, department_id FROM employees");
 $stmt->execute();
 
-// Fetching data
+// Fetching data one row at a time
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Department: " . $row['department'] . "<br>";
+    echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Department: " . $row['department_id'] . "<br>";
 }
 
-
+// Fetching data all at once
+//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//foreach ($results as $row) {
+//    echo "ID: " . $row['id'] . ", Name: " . $row['name'] . ", Department: " . $row['department_id'] . "<br>";
+//}
