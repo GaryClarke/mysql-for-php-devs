ed4da75b0eeb61c5a9eec22d2760360fedf7a6e1
Gary Clarke <garyclarke1974@gmail.com>
2024-11-11 14:02:52 +0000

    Create pdo-creating-challenge.php

--- PDO_CRUD/pdo-creating-challenge.php
+++ PDO_CRUD/pdo-creating-challenge.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 $db = \App\DB\Database::getInstance();
 $pdo = $db->getPDO();
 
-// Prepare an INSERT statement
+// Prepare an INSERT statement to create a new 'Design' department
 $stmt = $pdo->prepare("INSERT INTO department 
     (name) 
     VALUES (?)");
@@ -13,12 +13,11 @@ $data = ['Design'];
 
 // Execute the statement with data
 if ($stmt->execute($data)) {
-    echo "New record created successfully";
+    echo "New record created successfully" . PHP_EOL;
 } else {
     // A human-readable error message from the database driver.
     echo "Error: " . $stmt->errorInfo()[2];
 }
-
 
 
 
--- PDO_CRUD/pdo-creating.php
+++ PDO_CRUD/pdo-creating.php
@@ -6,18 +6,20 @@ $db = \App\DB\Database::getInstance();
 $pdo = $db->getPDO();
 
 // Prepare an INSERT statement
-$stmt = $pdo->prepare("INSERT INTO employees 
-    (name, age, salary, department_id, manager_id) 
-    VALUES (?, ?, ?, ?, ?)");
+$stmt = $pdo->prepare("INSERT INTO employees
+    (name, age, salary, department_id, manager_id)
+    VALUES (?,?,?,?,?)");
 $data = ['Batman', 30, 50000, 3, 16];
 
 // Execute the statement with data
 if ($stmt->execute($data)) {
-    echo "New record created successfully";
+    echo "New record created successfully" . PHP_EOL;
 } else {
     // A human-readable error message from the database driver.
     echo "Error: " . $stmt->errorInfo()[2];
 }
+
+
 
 
 
